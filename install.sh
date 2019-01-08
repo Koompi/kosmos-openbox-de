ed40421c9979c6ab83b7cead94fe0014c5b7250c
Brilliant PHAL <36073881+mrrbrilliant@users.noreply.github.com>
2019-01-08 14:38:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,58 +8,58 @@ cd ..
 
 
 ## Setup xserver
-sudo pacman -S xorg
+sudo pacman -S --noconfirm xorg
 
 ## Setup display manager
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm.service
 
 ## Setup window manager
-sudo pacman -S openbox obconf obmenu
+sudo pacman -S --noconfirm openbox obconf obmenu
 
 ## Setup terminal
-sudo pacman -S gnome-terminal xterm
+sudo pacman -S --noconfirm gnome-terminal xterm
 
 ## Setup desktop env components
-sudo pacman -S tint2 jgmenu plank nitrogen compton
+sudo pacman -S --noconfirm tint2 jgmenu plank nitrogen compton
 
 ## Setup file and disk management applications
-sudo pacman -S nautilus gnome-disk-utility gnome-disks xarchiver
+sudo pacman -S --noconfirm nautilus gnome-disk-utility gnome-disks xarchiver
 
 ## Setup editors
-sudo pacman -S gedit code atom
+sudo pacman -S --noconfirm gedit code atom
 
 ## Setup office
-sudo pacman -S libreoffice-fresh
+sudo pacman -S --noconfirm libreoffice-fresh
 
 ## Setup system setting tools
 ### Resource and Task Management
-sudo pacman -S  gnome-system-monitor
+sudo pacman -S --noconfirm gnome-system-monitor
 
 ### Authentication Manager
-sudo pacman -S polkit-gnome
+sudo pacman -S --noconfirm polkit-gnome
 
 ### Power Management
-sudo pacman -S mate-power-manager
+sudo pacman -S --noconfirm mate-power-manager
 
 ### Display Management
-sudo pacman -S arandr
+sudo pacman -S --noconfirm arandr
 
 ### Network Management
-sudo pacman -S wpa_supplicant dialog networkmanager nm-connection-editor network-manager-applet openssh rsync samba
+sudo pacman -S --noconfirm wpa_supplicant dialog networkmanager nm-connection-editor network-manager-applet openssh rsync samba
 #### VPN
-sudo pacman -S networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc
+sudo pacman -S --noconfirm networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc
 sudo systemctl enable NetworkManager.service
 
 ### Sound Manager
-sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf pavucontrol
+sudo pacman -S --noconfirm pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf pavucontrol
 yay -S --noconfirm volti
 
 ### Setup theme
-sudo pacman -S lxappearance-gtk3 paper-icon-theme materia-gtk-theme adapta-gtk-theme 
+sudo pacman -S --noconfirm lxappearance-gtk3 paper-icon-theme materia-gtk-theme adapta-gtk-theme 
 
 ### Setup printer
-sudo pacman -S system-config-printer cups-pk-helper
+sudo pacman -S --noconfirm system-config-printer cups-pk-helper
 yay -S --noconfirm gtklp
 
 ### File preference
